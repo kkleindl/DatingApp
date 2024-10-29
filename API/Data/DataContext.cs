861c1bbfec1cddcdea5e40f5489ee4c87d814475
kkleindl <kyle.kleindl@gmail.com>
2024-10-29 14:20:40 -0700

    End of Section 8

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,9 +7,4 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
-
-    public static implicit operator ControllerContext(DataContext v)
-    {
-        throw new NotImplementedException();
-    }
 }
